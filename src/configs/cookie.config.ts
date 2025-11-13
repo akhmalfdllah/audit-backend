59c3ad69d8573f787e07ce375b355e47f0a9f4ab
Akhmal Faidillah <akmal.faidillah@gmail.com>
2025-11-13 23:17:32 +0700

    same site none

--- src/configs/cookie.config.ts
+++ src/configs/cookie.config.ts
@@ -4,7 +4,7 @@ export default registerAs('cookie', () => ({
   name: process.env.COOKIE_NAME || 'refresh_token',
   secure: process.env.COOKIE_SECURE === 'true',
   httpOnly: true,
-  sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+  sameSite: process.env.NODE_ENV === 'none',
   path: '/',
   maxAge: parseInt(process.env.COOKIE_MAX_AGE || '604800000'), // 7 hari
 }));
